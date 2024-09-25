9b509763056c0790544aab2e577b76d805f6bff2
SrinivasRam <bala.koteswara@anywhere.co>
2024-09-25 21:24:58 +0530

    feat: update build assets path

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   publicDir: "./src/assets",
+  build: {
+    outDir: "./docs",
+    assetsDir: "fe-workshop-signmore"
+  }
 });
